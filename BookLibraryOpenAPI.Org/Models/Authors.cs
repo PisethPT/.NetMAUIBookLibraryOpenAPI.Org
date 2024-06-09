38ff57f188452f22de9ee3e4365541dfc0cf6e9f
YOS PISETH <201002@student.sru.edu.kh>
2024-06-09 22:03:27 +0700

    and flyout&navbar

--- BookLibraryOpenAPI.Org/Models/Authors.cs
+++ BookLibraryOpenAPI.Org/Models/Authors.cs
@@ -1,5 +1,4 @@
 ﻿
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BookLibraryOpenAPI.Org.Models;
 public class Authors
@@ -30,7 +29,7 @@ public class LastModified
 public class Root
 {
     public string title { get; set; }
-   // public List<Author> authors { get; set; }
+  //  public List<Author> authors { get; set; }
     public string key { get; set; }
     public Type type { get; set; }
     public List<int> covers { get; set; }
